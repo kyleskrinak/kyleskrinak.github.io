aa6f7d2d371ae21dd10f3fc7ae33f53b4e0882ba
kyleskrinak <kyleskrinak@users.noreply.github.com>
2022-02-04 16:32:40 +0000

    jekyll build from Action b7b93e5c8cd786b9ab9cc62c0944c13b95877c83

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,223 @@
+var store = [{
+        "title": "Static Sites&#58; Everything Old Is New Again",
+        "excerpt":"Despite being an ardent Drupal fan, I also love statically-served sites for many reasons, especially for the freely available static-site services that Duke and GitHub makes available. For now I’m getting up and running on Jekyll using the Minimal Mistakes theme. This theme has a lot of features baked in,...","categories": ["Personal productivity"],
+        "tags": [],
+        "url": "/personal%20productivity/first-blog-post/",
+        "teaser": null
+      },{
+        "title": "Vim for writers",
+        "excerpt":"Here’s an excellent article on using Vim for narrative writing, as opposed to technical documentation. I love the idea of an immediate editor for the kind of lazy checking I like to do before I share something I’ve written. While reading this article, I see attention drawn to NeoVim. Never...","categories": ["Personal productivity"],
+        "tags": ["Vim editor"],
+        "url": "/personal%20productivity/vim-for-writers/",
+        "teaser": null
+      },{
+        "title": "Drupal @ Duke meetup notes",
+        "excerpt":"Here are my hastily-assembled thoughts on Drupal 8 for 2017 at Trinity Technology Services.   To view this document on Sway    ","categories": ["Drupal"],
+        "tags": ["Drupal @ Duke"],
+        "url": "/drupal/duke-meetup/",
+        "teaser": null
+      },{
+        "title": "DrupalCon Baltimore 2017 &mdash; Backend Security notes",
+        "excerpt":"Duke University IT Security Office’s PDF is found here here Physical aspects: Access-controlled server rooms Authorized physical access only Log all access as required by data Network aspects: Appropriate Host-based access controls, i.e., firewall, port control, access lists) Require secure remote access, using VPN or SSH Encrypt all authentication traffic...","categories": ["Drupal"],
+        "tags": ["Drupalcon Baltimore 2017"],
+        "url": "/drupal/drupalcon-baltimore-2017-mdash-backend-security-notes/",
+        "teaser": null
+      },{
+        "title": "Drupal 8 Multisite documentation",
+        "excerpt":"Summary In order to better understand what adopting Drupal 8 multisite means, I have reviewed the code base for Drupal 8.3.2 for all references related to ‘multisite’ or multi-site’ in the comments of the Drupal code. Conceptually, Drupal 8 multisite seems identical to Drupal 7’s model, though there are noteworthy...","categories": ["Drupal"],
+        "tags": ["Multisite"],
+        "url": "/drupal/drupal-8-multisite-documentation/",
+        "teaser": null
+      },{
+        "title": "DrupalCon Nashville 2018 -- Higher Education Summit Notes",
+        "excerpt":"I will be leading two sessions at the DrupalCon Higher Education Summit on Monday, April 8th, 2018, and I’m using this post for my preparation notes. You can find all the virtual directory for all the Higher Education Summit Session notes here. Note: these agenda items are not hard-and-fast discussion...","categories": ["Drupal"],
+        "tags": ["Drupalcon Nashville 2018"],
+        "url": "/drupal/drupalcon-nashville-2018/",
+        "teaser": null
+      },{
+        "title": "DrupalCon Nashville 2018 -- 04-09-2018 Higher Ed Summit Day",
+        "excerpt":"Keynote Converting vb-script site to a modern, responsive heterogeneous site with Drupal 8 Good mix of heterogeneous systems to produce a modern and robust content website, including static pages This blog post echos much of this keynote discussion Accessibility Panel Training content editors Incentivize training? Clarifying roles for responsibility How...","categories": ["Drupal"],
+        "tags": ["Drupalcon Nashville 2018"],
+        "url": "/drupal/drupalcon-nashville-2018-04-09-2018-higher-ed-summit-day/",
+        "teaser": null
+      },{
+        "title": "DrupalCon Nashville 2018 -- 04-10-2018 Tuesday",
+        "excerpt":"Dries Keynote TL;DR: Back to roots New in 8.5 Media and content moderation Stable upgrade path to D8 Simplified site building API-first focus User facing changes to better address poor installation experience for new users who wish to try Drupal CMI changes, composer changes, removing it as a requirement, but...","categories": ["Drupal"],
+        "tags": ["Drupalcon Nashville 2018"],
+        "url": "/drupal/drupalcon-nashville-2018-04-10-2018-higher-ed-summit-day/",
+        "teaser": null
+      },{
+        "title": "DrupalCon Nashville 2018 -- 04-11-2018 Wednesday",
+        "excerpt":"Keynote: Drupal and the secret of my success — Steve Francia highlights Biography, startup failures Introducing Drupal for MongoDB site Emulating Drupal’s community techniques Hugo, Docker involvement Illness, life lessons Key for success Open Source Collaborative Development Community Engagement Steve’s message: Don’t stop Summary Easily the best non-Dries keynote I’ve...","categories": ["Drupal"],
+        "tags": ["Drupalcon Nashville 2018"],
+        "url": "/drupal/drupalcon-nashville-2018-04-11-2018-higher-ed-summit-day/",
+        "teaser": null
+      },{
+        "title": "DrupalCon Nashville 2018 Video Playlist",
+        "excerpt":"Below is my list of videos to catch up on DrupalCon Nashville 2018. The first is the youtube embed of my public playlist, followed by the exported links for non-js browsers. DrupalCon Nashville 2018: Driesnote DrupalCon Nashville 2018: Keynote: Drupal and the secret of my success | Steve Francia DrupalCon...","categories": ["Drupal"],
+        "tags": ["Drupalcon Nashville 2018"],
+        "url": "/drupal/drupalcon-nashville-2018-video-playlist/",
+        "teaser": null
+      },{
+        "title": "5 DrupalCon Nashville Take Aways",
+        "excerpt":"For our Drupal @ Duke meetup, here are my top five take-aways from this year’s DrupalCon Nashville. 1) Web accessibility Web accessibility has become front-and-center, both in academic shops, hence this opening panel, and commercial as we saw with later sessions. The Higher Ed Summit, which is easily my favorite...","categories": ["Drupal"],
+        "tags": ["Drupalcon Nashville 2018"],
+        "url": "/drupal/5-drupalcon-nashville-take-aways/",
+        "teaser": null
+      },{
+        "title": "From My Wife’s Kitchen",
+        "excerpt":"My wife completely supports my LCHF way of eating, even if she’s not doing it herself. She loves to cook. It’s her passion and the kitchen is her artist’s studio. With her guidance, and in my attempt to free her from additional work for my WoE, I’ve started to cook...","categories": ["LCHF"],
+        "tags": [],
+        "url": "/lchf/my-first-lchf-post/",
+        "teaser": null
+      },{
+        "title": "What is n=1?",
+        "excerpt":"I hadn’t heard of “n=1” before LCHF. I heard it over at the Ketogenic Forums, where it is a category for posts. Wikipedia describes it as; “An N of 1 trial is a clinical trial in which a single patient is the entire trial, a single case study.” Most informally,...","categories": ["LCHF"],
+        "tags": [],
+        "url": "/lchf/n-1/",
+        "teaser": null
+      },{
+        "title": "My Morning Routine",
+        "excerpt":"Quote by Tim Ferriss, image from inc.com My morning routine precedes my LCHF way of eating by about a year. My routine, itself, reflects the gradual development of decades of experience, self-reflection and formation. It helped me to be open to, and steel myself, for the change I needed to...","categories": ["LCHF","Personal productivity"],
+        "tags": [],
+        "url": "/lchf/personal%20productivity/my-morning-routine/",
+        "teaser": null
+      },{
+        "title": "How to Pick a Restaurant",
+        "excerpt":"Better Dining through Butter Who doesn't love the convenience of eating out? You can sit down, relax, and enjoy company – or alone time – while having others serve you drinks, snacks and then dinner. You're free to try new foods or drinks without renovating your kitchen. You can enjoy...","categories": ["LCHF"],
+        "tags": [],
+        "url": "/lchf/how-to-restaurant/",
+        "teaser": null
+      },{
+        "title": "A Pound of Flesh and a Hot Tub",
+        "excerpt":"At my age, losing 60 pounds will leave you with extra skin. Things don’t snap back like they used to. That’s fine by me, but it sets the stage for this story. We have a hot tub. Winters and hot tubs are simply awesome. My teenaged son never misses the...","categories": ["LCHF"],
+        "tags": ["Story-telling"],
+        "url": "/lchf/a-pound-of-flesh-and-a-hot-tub/",
+        "teaser": null
+      },{
+        "title": "bundle test",
+        "excerpt":"## Quote #1 “Friendship is born at that moment when one person says to another: ‘What! You too? I thought I was the only one.’” ―C.S. Lewis ## Quote #2 “Anybody can sympathize with the sufferings of a friend, but it requires a very fine nature to sympathize with a...","categories": [],
+        "tags": [],
+        "url": "/bundle-test/",
+        "teaser": null
+      },{
+        "title": "Old Again, New Again",
+        "excerpt":"I was a computer systems magazine production manager in the early ’90s, in NYC, at Hachette-Filipacchi Magazines. Their portfolio included Elle, Woman’s Day and Stereo Review, among other popular national magazines. There were a lot of celebrities about. It was a fantastic and exciting job for this young buck. My...","categories": ["LCHF","Personal productivity"],
+        "tags": [],
+        "url": "/lchf/personal%20productivity/old-again-new-again/",
+        "teaser": null
+      },{
+        "title": "2019-Feb-13 SLG Presentation",
+        "excerpt":"# From F to B #### in Mozilla’s Observatory Security Report The **Trinity Technology Services** system administrators implement changes to Varnish to dramatically improve our Drupal website’s security. * Kyle Skrinak * David Palmer * Blaine Ott https://people.duke.edu/~kds38/presentations/2019-Feb-SLG.html # The Problem * ITSO sends TTS NetSparker report * We break...","categories": [],
+        "tags": [],
+        "url": "/presentations/2019-Feb-SLG.html",
+        "teaser": null
+      },{
+        "title": "2019 DrupalCon Drupal 8 Multisite",
+        "excerpt":"# Our DevOps Approach to Multisite and Drupal 8 ---- ### A high-level overview of how we’re using DevOps to #### pimp our stack ## Kyle Skrinak, I.T. Manager Web Development and Systems Administration https://people.duke.edu/~kds38 _(nom de cyber: @screenack)_ ---- #### Trinity College of Arts &amp; Sciences _at_ ### Duke...","categories": [],
+        "tags": [],
+        "url": "/presentations/DC19-DevOps-Multisite.html",
+        "teaser": null
+      },{
+        "title": "2019 DrupalCon Higher Ed Summit",
+        "excerpt":"The Higher Ed Summit organizers had considerably redesigned the schedule and makeup of the summit. I say to a significantly improved format. This year, we: Introductions and the usual keynote start Held three sessions of three concurrent presentations, broken out by interest, i.e., marketing or back-end Lunch A “Solutions” meeting...","categories": ["Drupal"],
+        "tags": ["Drupalcon Seattle 2019"],
+        "url": "/drupal/2019-drupalcon-higher-ed-summit/",
+        "teaser": null
+      },{
+        "title": "How to Manage Departmental Faculty and Staff Data",
+        "excerpt":"# How to Manage Departmental Faculty and Staff Data ---- #### This presentation: https://bit.ly/2020-04-OpenOffice ## Kyle Skrinak, I.T. Manager Web Development and Systems Administration https://people.duke.edu/~kds38 #### Trinity College of Arts &amp; Sciences (TCA&S), _now part of OIT_, ### Duke University ---- #### This presentation: https://bit.ly/2020-04-OpenOffice # What we’re covering today...","categories": [],
+        "tags": [],
+        "url": "/presentations/19-07-tts-profile.html",
+        "teaser": null
+      },{
+        "title": "Diminished Zeal with Steady Commitment",
+        "excerpt":"After 2+ years, the loss continues Though my LCHF journaling has been quiet, don’t let my silence fool you. I have been thoroughly enjoying low-carb living and its consequences. My initial excitement was my finding actual validation in what I had read in Atkins diet books fifteen or more years...","categories": ["LCHF"],
+        "tags": [],
+        "url": "/lchf/diminished-zeal-with-steady-commitment/",
+        "teaser": null
+      },{
+        "title": "My Windows 10 Setup",
+        "excerpt":"The Platform Nomad Every four years, Duke University personnel receive a “device refresh,” i.e., a new desktop or laptop device. To celebrate my fourth year at Duke, I switched from a MacBook to a Lenovo Yoga laptop. Though I remain an admirer of the life and achievements of Steve Jobs,...","categories": ["Personal productivity"],
+        "tags": [],
+        "url": "/personal%20productivity/my-windows-10-setup/",
+        "teaser": null
+      },{
+        "title": "Don't You Miss Carbs?",
+        "excerpt":"It’s a common question for me these days. After talking about my weight-loss, severely reducing my carb intake, and how I must remain low-carb to do so, my interlocutor nearly always will press me; Don’t you miss the carbs? Honest Answer: Emotionally? OMG Yes! Rationally? Not in the least. Here’s...","categories": ["LCHF"],
+        "tags": [],
+        "url": "/lchf/don-t-you-miss-carbs/",
+        "teaser": null
+      },{
+        "title": "Lorraine Barbara Kubik Skrinak",
+        "excerpt":"“Did you know you brighten my day? You are like sunshine to me.” This is how Mom greeted my daily visits at rehab. This was the essence of Lorraine Barbara Kubik Skrinak. She radiated an authentic, and charismatic love to you, whether you where her son, or the maintenance guy...","categories": ["Personal"],
+        "tags": [],
+        "url": "/personal/lorraine-barbara-kubik-skrinak/",
+        "teaser": null
+      },{
+        "title": "Introduction to Drupal",
+        "excerpt":"# Introduction to Drupal ---- #### This presentation: https://bit.ly/tts-drupal-intro # Your presenter ---- ## Kyle Skrinak, I.T. Manager Web Development and Systems Administration https://people.duke.edu/~kds38 #### Trinity College of Arts &amp; Sciences _at_ ### Duke University # What we’re covering today ---- * Editing content at TCA&S using Drupal * Managing...","categories": [],
+        "tags": [],
+        "url": "/presentations/20-03-05-drupal-intro.html",
+        "teaser": null
+      },{
+        "title": "Happy Third Lowcarbiversary",
+        "excerpt":"Left, working the camera at my son’s football game, 2014-August. Right, 2020-June. Yo-yo dieting, no mas I’ve lost more weight in the past. I became frail, cold and hungry on those diets. I’ve never kept it off for more than three months. I never felt as good as I do...","categories": ["LCHF"],
+        "tags": [],
+        "url": "/lchf/happy-third-lowcarbiversary/",
+        "teaser": null
+      },{
+        "title": "Drupal Multisite on a Dime",
+        "excerpt":"# Drupal Multisite on a Dime ## or How we Adopted CI/CD for TCA&S’s Drupal Multisite Hosting for Trinity’s 100+ Drupal websites ---- #### This presentation: https://bit.ly/tcas-drupal-multisite # Your presenter ---- ## Kyle Skrinak, I.T. Manager ### Trinity College of Arts & Sciences Web Support (now part of OIT), Duke...","categories": [],
+        "tags": [],
+        "url": "/presentations/20-12-04-drupal-multisite.html",
+        "teaser": null
+      },{
+        "title": "Jekyll, Hugo, and Me",
+        "excerpt":"I’ve been using Jekyll for the blog that you’re currently reading for several years now. There is much to love in static websites for my professional and personal musings. My employer offers free static website hosting, and as a manager, I can use my blog pages for commonly-asked-for content or...","categories": ["Static Websites"],
+        "tags": [],
+        "url": "/static%20websites/jekyll-hugo-and-me/",
+        "teaser": null
+      },{
+        "title": "Two Guys Watch a Burning House, Part I",
+        "excerpt":"Note: I originally posted this on my Facebook account on March 19th, 2017. However, Facebook is getting rid of their “notes” blog-like ability, so I’ve moved this content to my blog, with some edits for clarity. I forget when in 1973 the Skrinak family house caught on fire. I’m guessing...","categories": ["Personal"],
+        "tags": ["Story-telling"],
+        "url": "/personal/two-guys-watch-a-burning-house/",
+        "teaser": null
+      },{
+        "title": "My Hero, Karen &mdash; Part II",
+        "excerpt":"Note: I originally posted this on my Facebook account on March 26th, 2017. However, Facebook is getting rid of their “notes” blog-like ability, so I’ve moved this content to my blog, with some edits for clarity. This is the conclusion to “Two Guys Watch a Burning Building.” I like this...","categories": ["Personal"],
+        "tags": ["Story-telling"],
+        "url": "/personal/my-hero-karen/",
+        "teaser": null
+      },{
+        "title": "Shinleaf Campsite",
+        "excerpt":"Note: I originally posted this on my Facebook account on November 8th, 2008. However, Facebook is getting rid of their “notes” blog-like ability, so I’ve moved this content to my blog, with some edits for clarity. A tall and thick web of branch silhouettes cut the night sky. A few...","categories": ["Personal"],
+        "tags": ["Story-telling"],
+        "url": "/personal/shinleaf-campsite/",
+        "teaser": null
+      },{
+        "title": "Meet Holly",
+        "excerpt":"HOLLY - ID#A054700 Note: I originally posted this on my Facebook account on August 1st, 2009. However, Facebook is getting rid of their “notes” blog-like ability, so I’ve moved this content to my blog, with some edits for clarity. This was my post the day after we adopted Holly from...","categories": ["Personal"],
+        "tags": ["Story-telling"],
+        "url": "/personal/meet-holly/",
+        "teaser": null
+      },{
+        "title": "Gratitude, and That's Right",
+        "excerpt":"Note: I originally posted this on my Facebook account on April 2nd, 2017. However, Facebook is getting rid of their “notes” blog-like ability, so I’ve moved this content to my blog, with some edits for clarity. You all have been so kind about the two recent notes I’ve published. I’ve...","categories": ["Personal"],
+        "tags": ["Story-telling"],
+        "url": "/personal/gratitude-and-that-s-right/",
+        "teaser": null
+      },{
+        "title": "Loose Shorts and the TSA",
+        "excerpt":"I wrote this story, years ago, back when my low-carb weight loss was still new to me. Like many life-long yo-yo dieters, I had yet to develop the conviction that this time is different, and that I might once again re-inflate. The shorts in question were 4 sizes too big...","categories": ["Personal"],
+        "tags": ["Story-telling"],
+        "url": "/personal/loose-shorts-and-the-tsa/",
+        "teaser": null
+      },{
+        "title": "In the Jekyll garden",
+        "excerpt":"Continuing my research on a static website workflow, I’m spinning up a new personal website as an excuse to try building a new Jekyll-based static website. Doing so reveals the rough edges. How so? I’m trying a new Jekyll theme, Basically Basic, made by the same developer, Michael Rose, for...","categories": [],
+        "tags": [],
+        "url": "/in-the-jekyll-garden/",
+        "teaser": null
+      }]
